90bf4df08816e7687d40126f995b1846dee0f391
airineidaniel23 <airineidaniel23@gmail.com>
2022-12-22 23:43:52 +0200

    Add physics to goblin

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ console.log("Server started");
 var socket_list = {};
 var player_list = {};
 var pack = [];
+
+const width = 500;
+const height = 500;
+const GRID_WIDTH = 10;
+const GRID_HEIGHT = 10;
+
 var Player = function(id) {
     var self = {
         x: 250,
@@ -23,26 +29,52 @@ var Player = function(id) {
         pL: false,
         pU: false,
         pD: false,
+        mouseX: 251,
+        mouseY: 250,
+        jumping: false,
         facingLeft: false,
+        speed: 0,
+        acc: 1,
         maxSpeed: 10  
     }
     self.updatePosition = function() {
-        if(self.pR) {
+        if(self.pR && self.x < width)
             self.x += self.maxSpeed;
-            self.facingLeft = false;
-        }
-        if(self.pL) {
+        if(self.pL && self.x > 0)
             self.x -= self.maxSpeed;
-            self.facingLeft = true;
+        if(self.pU && self.jumping == false) {
+            self.speed = - 10;
+            self.acc = 1;
         }
-        if(self.pU)
-            self.y -= self.maxSpeed;
-        if(self.pD)
-            self.y += self.maxSpeed;
-            
+        if (self.mouseX < self.x)
+            self.facingLeft = true;
+        else self.facingLeft = false;
+        if (self.y > width - 100) {
+            if (self.jumping == true) {
+                self.jumping = false;
+                self.acc = 0;
+                self.speed = 0;
+            }
+        } else if (self.jumping == false) {
+            self.jumping = true;
+        }
+        self.speed += self.acc;
+        self.y += self.speed;
     }
     return self;
 }
+
+let ground = [];
+for (let i = 0; i < GRID_HEIGHT; i++) {
+  ground[i] = [];
+  for (let j = 0; j < GRID_WIDTH; j++) {
+    ground[i][j] = {
+      mined: false,  // Whether this tile has been mined or not
+      value: 0       // The value of this tile (e.g. ore amount)
+    };
+  }
+}
+
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket) {
     console.log("socket connection");
@@ -66,6 +98,11 @@ io.sockets.on('connection', function(socket) {
             player.pU = data.state;
         else if(data.inputId == "down")
             player.pD = data.state; 
+    });
+
+    socket.on('mouseMoved', function(data) {
+        player.mouseX = data.x;
+        player.mouseY = data.y;
     });
 });
 
